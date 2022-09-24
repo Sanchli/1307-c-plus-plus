90233903fd29927098f57bdb91fea49a66400a86
Edgar Sanchez <sanchedg020213@gmail.com>
2022-09-24 17:28:09 -0500

    save

---
+++ Lab5/HOTCOLD/HOTCOLD.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    double temperature;
+
+    cout << "Freezing and Boilding Points" << endl;
+    cout << "Input a Random Temperature in Farenheit: " << endl;
+    cin >> temperature;
+
+    if (temperature > -173)
+        cout <<Ethyl Alchohol 
+
+}
