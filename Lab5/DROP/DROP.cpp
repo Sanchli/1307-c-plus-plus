574495fb7dc30d7e246dbf321c563bb65dc1df01
Edgar Sanchez <sanchedg020213@gmail.com>
2022-09-24 19:39:29 -0500

    save drop

--- Lab5/DROP/DROP.cpp
+++ Lab5/DROP/DROP.cpp
@@ -1,13 +1,33 @@
 	#include <cstdlib>
 	#include <iostream>
 
-	using namspace std;
+	using namespace std;
 
 	int main()
 	{
+		string student_name;
+		double grade1,grade2,grade3;
+		
+		cout << "Enter student's name: ";
+		cin >> student_name;
+
+		cout << "Enter 3 grades" << endl << endl;
+		
+		cout << "First grade: ";
+		cin >> grade1;
+		cout << "Second grade: ";
+		cin >> grade2;
+		cout << "Thirds grade: ";
+		cin >> grade3;
+
+		if (grade1 < 0 && grade1 > 100)
+		{
+			grade1
+		}
+		
 
 
 
-		system(PAUSE);
+		system("PAUSE");
 		return 0;
 	}
