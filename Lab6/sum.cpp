3d42a087cd57fd1e2afa16ada6c5a9ff840135c8
Edgar Sanchez <Sanchedg020213@gmail.com>
2022-09-28 12:28:58 -0500

    added comments to lab 6

--- Lab6/sum.cpp
+++ Lab6/sum.cpp
@@ -9,9 +9,11 @@ int main ()
 
     while (c <= 100)
     {
+        //Will add  to sum and increment by one every time the loop runs until it arrives to 100
         sum = sum + c;
         c = c + 1;
     }
+    //Ouput the total
     cout << "The sum all number between 1-100 is " << sum << endl;
     system ("PAUSE");
     return 0;
