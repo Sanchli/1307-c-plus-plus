55d01d0c733efbebeab94c83bfcdb6f08115d47a
Edgar Sanchez <Sanchedg020213@gmail.com>
2022-09-28 11:25:15 -0500

    tempchart creation

--- Lab6/sum.cpp
+++ Lab6/sum.cpp
@@ -5,15 +5,14 @@ using namespace std;
 
 int main ()
 {
-    int c = 0;
-    int sum = 0;
+    int c = 0, sum = 0;
 
-    while (c < 100)
+    while (c <= 100)
     {
         sum = sum + c;
         c = c + 1;
     }
-    cout << "The sum for the 1-100 is " << sum << endl;
+    cout << "The sum all number between 1-100 is " << sum << endl;
     system ("PAUSE");
     return 0;
 }
---
+++ Lab6/tempchart.cpp
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int celsius = 0, fahrenheit;
+    cout << "Temperature Converter" << endl;
+    cout << "Celsius"<< "       " << "Fahrenheit" << endl;
+
+    while(celsius <= 100)
+    {
+        fahrenheit = celsius * 1.8 + 32;
+    }
+}
