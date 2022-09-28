a17816fef592a188241d119a0aa89c9f9df39f73
Edgar Sanchez <Sanchedg020213@gmail.com>
2022-09-28 11:31:39 -0500

    tempchart finished

--- Lab6/tempchart.cpp
+++ Lab6/tempchart.cpp
@@ -6,11 +6,13 @@ using namespace std;
 int main()
 {
     int celsius = 0, fahrenheit;
-    cout << "Temperature Converter" << endl;
-    cout << "Celsius"<< "       " << "Fahrenheit" << endl;
+    cout << "Temperature Chart" << endl;
+    cout << "Celsius"<< "     " << "Fahrenheit" << endl;
 
     while(celsius <= 100)
     {
         fahrenheit = celsius * 1.8 + 32;
+        cout << celsius << "              " << fahrenheit << endl;
+        celsius = celsius + 10 ;
     }
 }
