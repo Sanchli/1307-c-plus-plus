3f949a79c71f785f4a1f1c19bb744ebc7bfea058
Edgar Sanchez <Sanchedg020213@gmail.com>
2022-10-06 22:19:33 -0500

    finished pumkin

---
+++ Lab7/build/pumpkin.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    system("clear");
+    int pumpkins = 0;
+    double weight,total, average;
+
+    cout << "Pumpking weight average" << endl;
+
+    //Prompts user for pumpkins weights
+    cout << "Enter the weights of all your pumpkins(0 to exit):";
+    cin >> weight;
+
+
+//for loop will add all pumpkin weights together and counts pumpkins for a total.
+//Sentinel loop will continue to run until 0 is entered. 
+for (weight; weight != 0; pumpkins++)
+{
+    total = total + weight;
+    cout << "Enter next pumkin weight(0 to exit)" << endl;
+    cin >> weight;
+}
+
+//average equation with precision setting making sure result only print two decimal places. 
+average = total / pumpkins;
+cout << setiosflags(ios::fixed);
+cout << setprecision(2);
+
+//Outputs the result of pumpkin average
+cout << "The average for " << pumpkins <<" pumpkins is " << average << " pounds." << endl;
+return 0;    
+system("PAUSE");
+
+
+
+}
