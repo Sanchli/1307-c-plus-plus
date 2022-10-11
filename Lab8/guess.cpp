341249d5f0b33864e27ada9680a9ec457fa74b3e
Edgar Sanchez <sanchedg020213@gmail.com>
2022-10-11 14:28:28 -0500

    created files

---
+++ Lab8/guess.cpp
@@ -0,0 +1,6 @@
+//(guess) Write a program that accepts a number as a guess between 1 an 100 as input and outputs if the guess is "too high", 
+// "too low", or "correct". When the guess is correct the program should output how many guesses it took.
+//10 points - correct use of loop statement
+//10 points - correct use of if statements
+//10 points - correct #include statements
+//10 points - correct outputs 
