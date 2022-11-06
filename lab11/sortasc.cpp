8f5c94a5ad6b33c2fe381eba8f883df0f7e1bb99
Edgar Sanchez <sanchedg020213@gmail.com>
2022-11-06 15:24:17 -0600

    finished lab11

---
+++ lab11/sortasc.cpp
@@ -0,0 +1,39 @@
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+    int array_size = 10;
+    int nums[array_size];
+    int minValue,minIndex,temp;
+
+
+    for(int p=0;p<10;p++)
+    {
+        std::cout << "Input some numbers: " << std::endl;
+        std::cin >> nums[p];
+    }
+
+    for(int j=0; j < (array_size -1); j++)
+    {
+        minIndex = j;
+        for(int i = j+1; i < array_size; i++)
+            {
+                if(nums[i] < nums[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+        if(minIndex != j)
+        {
+            temp = nums[minIndex];
+            nums[minIndex] = nums[j];
+            nums[j] = temp;
+        }
+    }
+    for (int t=9; t >= 0; t--) 
+    {
+        std::cout << nums[t]<< " ";
+    }   
+
+}
--- lab11/sortdes.cpp
+++ lab11/sortdes.cpp
@@ -3,11 +3,37 @@
 
 int main()
 {
-    int num[10];
+    int array_size = 10;
+    int nums[array_size];
+    int minValue,minIndex,temp;
 
-    for(int i=0;i<10;i++)
+
+    for(int p=0;p<10;p++)
     {
         std::cout << "Input some numbers: " << std::endl;
-        std::cin >> num[i];
+        std::cin >> nums[p];
     }
+
+    for(int j=0; j < (array_size -1); j++)
+    {
+        minIndex = j;
+        for(int i = j+1; i < array_size; i++)
+            {
+                if(nums[i] < nums[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+        if(minIndex != j)
+        {
+            temp = nums[minIndex];
+            nums[minIndex] = nums[j];
+            nums[j] = temp;
+        }
+    }
+    for (int t=0; t < array_size; t++) 
+    {
+        std::cout << nums[t]<< " ";
+    }   
+
 }
